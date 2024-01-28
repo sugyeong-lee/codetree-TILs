faa37ea0b19d7c6551c407ea858f8d1960838045
sugyeong-lee <101383749+sugyeong-lee@users.noreply.github.com>
2024-01-29 00:44:21 +0900

    Title: Date to Date, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240129/Date to Date/date-to-date.py
+++ 240129/Date to Date/date-to-date.py
@@ -6,14 +6,15 @@ elapsed_days = 0
 
 
 while True:
-    if month == m2 and day == d2:
-        break
 
     elapsed_days += 1
     day += 1
 
-    if day > num_day[month]:
+    if day == num_day[month]:
         month += 1
         day = 1
 
+    if month == m2 and day == d2:
+        break
+        
 print(elapsed_days)
