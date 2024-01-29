07ee00c685f3bb1de959efb8c6a9056235abaeda
sugyeong-lee <101383749+sugyeong-lee@users.noreply.github.com>
2024-01-29 22:14:39 +0900

    Title: 십진수와 이진수 2, Time: 125ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240129/십진수와 이진수 2/decimal-and-binary-number-2.py
@@ -0,0 +1,21 @@
+binary = list(map(int, list(input())))
+length = len(binary)
+
+num = 0
+for i in range(length):
+    num = num * 2 + binary[i]
+
+num *= 17
+
+digits = []
+
+while True:
+    if num < 2:
+        digits.append(num)
+        break
+
+    digits.append(num % 2)
+    num //= 2
+
+for digit in digits[::-1]:
+    print(digit, end="")
